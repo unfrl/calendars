e4734d2054952f66aae3157b6cd91d2eb8913826
MattInternet <matt@kaijucode.com>
2019-12-19 19:18:56 -0700

    added ioredis stuff

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -3,9 +3,20 @@ import { Module } from "@nestjs/common";
 import { CalendarController } from "./controllers";
 import { CalendarService } from "./services";
 
+import * as IORedis from "ioredis";
+
 @Module({
     imports: [],
     controllers: [CalendarController],
-    providers: [CalendarService],
+    providers: [
+        CalendarService,
+        {
+            provide: IORedis,
+            useValue: new IORedis({
+                host: "localhost",
+                port: 6379,
+            }),
+        },
+    ],
 })
 export class AppModule {}
